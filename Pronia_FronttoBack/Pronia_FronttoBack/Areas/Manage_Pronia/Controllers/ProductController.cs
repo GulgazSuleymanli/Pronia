4332e58a9796c9ea853b9599caafe8677b981f04
GulgazSuleymanli <bd7tk5y26@code.edu.az>
2023-11-27 05:06:39 +0400

    Updated Product CRUD

--- Pronia_FronttoBack/Pronia_FronttoBack/Areas/Manage_Pronia/Controllers/ProductController.cs
+++ Pronia_FronttoBack/Pronia_FronttoBack/Areas/Manage_Pronia/Controllers/ProductController.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Pronia_FronttoBack.Areas.Manage_Pronia.ViewModels;
+﻿using Pronia_FronttoBack.Areas.Manage_Pronia.ViewModels;
+using Pronia_FronttoBack.Migrations;
 
 namespace Pronia_FronttoBack.Areas.Manage_Pronia.Controllers
 {
@@ -31,6 +31,9 @@ namespace Pronia_FronttoBack.Areas.Manage_Pronia.Controllers
             CreateProductVM productVM = new CreateProductVM();
 
             productVM.Categories = await _context.Categories.ToListAsync();
+            productVM.Colors = await _context.Colors.ToListAsync();
+            productVM.Sizes = await _context.Sizes.ToListAsync();
+            productVM.Tags = await _context.Tags.ToListAsync();
 
             return View(productVM);
         }
@@ -44,6 +47,9 @@ namespace Pronia_FronttoBack.Areas.Manage_Pronia.Controllers
             if (!ModelState.IsValid)
             {
                 productVM.Categories = await _context.Categories.ToListAsync();
+                productVM.Colors = await _context.Colors.ToListAsync();
+                productVM.Sizes = await _context.Sizes.ToListAsync();
+                productVM.Tags = await _context.Tags.ToListAsync();
 
                 return View(productVM);
             }
@@ -52,6 +58,9 @@ namespace Pronia_FronttoBack.Areas.Manage_Pronia.Controllers
             {
                 ModelState.AddModelError("CategoryId", "At least one Category must be selected");
                 productVM.Categories = await _context.Categories.ToListAsync();
+                productVM.Colors = await _context.Colors.ToListAsync();
+                productVM.Sizes = await _context.Sizes.ToListAsync();
+                productVM.Tags = await _context.Tags.ToListAsync();
 
                 return View(productVM);
             }
@@ -60,6 +69,9 @@ namespace Pronia_FronttoBack.Areas.Manage_Pronia.Controllers
             {
                 ModelState.AddModelError("CategoryId", "No such category exists");
                 productVM.Categories = await _context.Categories.ToListAsync();
+                productVM.Colors = await _context.Colors.ToListAsync();
+                productVM.Sizes = await _context.Sizes.ToListAsync();
+                productVM.Tags = await _context.Tags.ToListAsync();
 
                 return View(productVM);
             }
@@ -71,7 +83,91 @@ namespace Pronia_FronttoBack.Areas.Manage_Pronia.Controllers
                 ProductCode = productVM.ProductCode,
                 Price = productVM.Price,
                 CategoryId = productVM.CategoryId,
+                PrdColors = new List<ProductColor>(),
+                PrdSizes = new List<ProductSize>(),
+                PrdTags = new List<ProductTag>()
             };
+
+            if(productVM.ColorIds != null)
+            {
+                foreach (var colorid in productVM.ColorIds)
+                {
+                    Color color = await _context.Colors.FirstOrDefaultAsync(x => x.Id == colorid);
+
+                    if(color == null)
+                    {
+                        ModelState.AddModelError("ColorIds", "No such color exists");
+                        productVM.Categories = await _context.Categories.ToListAsync();
+                        productVM.Colors = await _context.Colors.ToListAsync();
+                        productVM.Sizes = await _context.Sizes.ToListAsync();
+                        productVM.Tags = await _context.Tags.ToListAsync();
+
+                        return View(productVM);
+                    }
+
+                    ProductColor productColor = new ProductColor()
+                    {
+                        ColorId = colorid,
+                        Product = product
+                    };
+
+                    product.PrdColors.Add(productColor);
+                }
+            }
+
+            if (productVM.SizeIds != null)
+            {
+                foreach (var sizeid in productVM.SizeIds)
+                {
+                    Size size = await _context.Sizes.FirstOrDefaultAsync(x => x.Id == sizeid);
+
+                    if (sizeid == null)
+                    {
+                        ModelState.AddModelError("SizeIds", "No such size exists");
+                        productVM.Categories = await _context.Categories.ToListAsync();
+                        productVM.Colors = await _context.Colors.ToListAsync();
+                        productVM.Sizes = await _context.Sizes.ToListAsync();
+                        productVM.Tags = await _context.Tags.ToListAsync();
+
+                        return View(productVM);
+                    }
+
+                    ProductSize productSize = new ProductSize()
+                    {
+                        SizeId = sizeid,
+                        Product = product
+                    };
+
+                    product.PrdSizes.Add(productSize);
+                }
+            }
+
+            if (productVM.TagIds != null)
+            {
+                foreach (var tagid in productVM.TagIds)
+                {
+                    Tag tag = await _context.Tags.FirstOrDefaultAsync(x => x.Id == tagid);
+
+                    if (tag == null)
+                    {
+                        ModelState.AddModelError("TagIds", "No such tag exists");
+                        productVM.Categories = await _context.Categories.ToListAsync();
+                        productVM.Colors = await _context.Colors.ToListAsync();
+                        productVM.Sizes = await _context.Sizes.ToListAsync();
+                        productVM.Tags = await _context.Tags.ToListAsync();
+
+                        return View(productVM);
+                    }
+
+                    ProductTag productTag = new ProductTag()
+                    {
+                        TagId = tagid,
+                        Product = product
+                    };
+
+                    product.PrdTags.Add(productTag);
+                }
+            }
 
             await _context.Products.AddAsync(product);
             await _context.SaveChangesAsync();
@@ -129,21 +225,6 @@ namespace Pronia_FronttoBack.Areas.Manage_Pronia.Controllers
 
             return RedirectToAction(nameof(Index));
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
         public async Task<IActionResult> Delete(int id)
         {
--- Pronia_FronttoBack/Pronia_FronttoBack/Areas/Manage_Pronia/ViewModels/Product/CreateProductVM.cs
+++ Pronia_FronttoBack/Pronia_FronttoBack/Areas/Manage_Pronia/ViewModels/Product/CreateProductVM.cs
@@ -9,5 +9,11 @@
         public double Price { get; set; }
         public int CategoryId { get; set; }
         public List<Category>? Categories { get; set; }
+        public List<int>? ColorIds { get; set; }
+        public List<Color>? Colors { get; set; }
+        public List<int>? SizeIds { get; set; }
+        public List<Size>? Sizes { get; set; }
+        public List<int>? TagIds { get; set; }
+        public List<Tag>? Tags { get; set; }
     }
 }
